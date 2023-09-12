12b78a04b49afb8e72b8a604145ce3168983bca0
Ravinder singh <rawatravi2012@gmail.com>
2023-09-12 11:39:54 +0000

    changes

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,5 @@
 export default {
-    HOST: "mongodb+srv://Ravi:ravi%40123@cluster0.cuq8f5k.mongodb.net/?retryWrites=true&w=majority",
+    HOST: "",
     PORT: 27017,
     DB: "fact_checker_db"
   };
